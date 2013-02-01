5f112e85c01ab10af9832bd95d80e9877f9912e3
Sam Decrock <sam.decrock@gmail.com>
2013-02-01 10:53:36 +0100

    added kabouter wesley

--- app/index.js
+++ app/index.js
@@ -7,10 +7,18 @@ $(function(){
 	htracker.init(videoInput, canvasInput);
 	htracker.start();
 
+	function handleMovement(e){
+		var head_x = e.originalEvent.x;
+		var head_y = e.originalEvent.y / 2;
+	}
+
 	// van hier gepikt:
 	// https://github.com/alexhancock/street-facing/blob/master/js/views/main.js
 	$(document).on('headtrackingEvent', function(e){
-		console.log(e.originalEvent.x + "," + e.originalEvent.y + "," + e.originalEvent.z);
+
+		//console.log(e.originalEvent.x + "," + e.originalEvent.y + "," + e.originalEvent.z);
+
+		_.throttle(handleMovement, 20);
 	});
 
 });
